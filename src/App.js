30a75f8b1e8b98610e8af8a141c7be9ba7f88dd8
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-12-07 23:28:21 +0530

    Footer Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Slider from "./components/Slider";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Header />
     <Banner />
     <Slider />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className="footer" style={{width:'100vw',height:'30vh',backgroundColor:'#2874f0', color:'white',textAlign:'center',display:"flex",justifyContent:'center',alignItems:'center'}}>
+        <h3>Ecommerce.com Copyright &#169; 2022-2023</h3>
+    </div>
+  )
+}
